f55747b65f2fba884b6489c90a69fb1ca3d7f836
Anjana Chamarthi <33385061+chamartea@users.noreply.github.com>
2021-07-21 12:25:06 -0400

    Delete gatsby-config.js

--- gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Duke Center for Law & Tech - Tech Project Analysis",
-  },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
-};
